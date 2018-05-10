eac1829ca8f453c7b235951ac1227f52eba031c9
Maxim <freepvps@gmail.com>
2018-05-11 02:27:41 +0300

    dataset and complete code

---
+++ src/indexmaker/__init__.py
@@ -0,0 +1,31 @@
+import model
+import os
+import argparse
+import json
+
+
+def dir_search(path):
+    yield path
+    if os.path.isdir(path):
+        sub = os.listdir(path)
+        for x in sub:
+            for r in dir_search(os.path.join(path, x)):
+                yield r
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", required=True, dest="input_dir", help="Input dir path")
+    parser.add_argument("-o", required=True, dest="output_file", help="Output json file")
+    args = parser.parse_args()
+
+    codes = []
+    for path in dir_search(args.input_dir):
+        print(path)
+        if path.lower().endswith("java") and os.path.isfile(path):
+            with open(path) as f:
+                codes.append(f.read())
+    index = model.make_index(codes)
+
+    with open(args.output_file, "w") as f:
+        json.dump(index, f)
---
+++ src/indexmaker/model.py
@@ -0,0 +1,45 @@
+import types
+import math
+import numpy as np
+import code2features
+from gensim.models.word2vec import Word2Vec
+
+
+def softmax(v):
+    exp_v = np.exp(v)
+    return exp_v / np.sum(exp_v, axis=0)
+
+
+def distance_to_probability(v, sigm=1.0):
+    v = np.asarray(v)
+    return softmax(-v * v / (sigm * sigm))
+
+
+def make_index(sentences):
+    """
+    :type sentences: list[basestring]
+    :param sentences: input code
+    :return: list of numpy vectors
+    :rtype: list[np.ndarray]
+    """
+    features = [map(str, code2features.extract_features(sentence)) for sentence in sentences]
+    w2v_model = Word2Vec(features, size=2, window=7, min_count=2, workers=1)
+
+    vectors = map(np.array, w2v_model.wv.vectors)
+    labels = [int(w2v_model.wv.index2word[i]) for i in range(len(vectors))]
+
+    features_size = code2features.TokenType.size
+    features_vecs = [None] * features_size
+    for i, v in enumerate(vectors):
+        features_vecs[labels[i]] = v
+
+    dist_matrix = np.zeros((features_size, features_size), dtype=np.float32)
+    for i, v1 in enumerate(features_vecs):
+        for j, v2 in enumerate(features_vecs):
+            if features_vecs[i] is None or features_vecs[j] is None:
+                dist_matrix[i][j] = 10.0
+            else:
+                dist_matrix[i][j] = np.linalg.norm(v1 - v2)
+    probs = [distance_to_probability(v) for v in dist_matrix]
+    return map(np.ndarray.tolist, probs)
+
