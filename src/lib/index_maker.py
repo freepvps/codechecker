up gitignore, fix sigm calculation, change tokens

src/lib/index_maker.py
@@ -27,9 +27,6 @@ def make_index(sentences):
     :return: list of numpy vectors
     :rtype: list[np.ndarray]
     """
-
-    sigm = math.sqrt(10)
-
     features = [map(str, code2features.extract_features(sentence)) for sentence in sentences]
     w2v_model = Word2Vec(features, size=30, window=20, min_count=2, workers=1)
 
@@ -42,6 +39,7 @@ def make_index(sentences):
         features_vecs[labels[i]] = v
 
     dist_matrix = np.zeros((features_size, features_size), dtype=np.float32)
+    dists_list = []
     for i, v1 in enumerate(features_vecs):
         for j, v2 in enumerate(features_vecs):
             if features_vecs[i] is None and features_vecs[j] is None:
@@ -50,8 +48,11 @@ def make_index(sentences):
                 dist_matrix[i][j] = 100000.0
             else:
                 dist_matrix[i][j] = np.linalg.norm(v1 - v2)
+                dists_list.append(dist_matrix[i][j])
+    sigm = math.sqrt(np.var(dists_list))
+
     probs = [distance_to_probability(v, sigm) for v in dist_matrix]
     flatten_probs = np.array(probs).flatten()
     res = np.ndarray.tolist(flatten_probs)
+
     return res
-    #return [f for i, f in enumerate() if feature_importance[i]]