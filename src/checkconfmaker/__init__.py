72f0cc78c5c18dace197977d3892b3729163a6f5
Maxim <freepvps@gmail.com>
2018-05-13 06:16:06 +0300

    validation add and refactor

--- src/checkconfmaker/__init__.py
+++ src/checkconfmaker/__init__.py
@@ -6,10 +6,10 @@ import lib
 import argparse
 import numpy as np
 import tensorflow as tf
+import random
 
 from tensorflow.python.client import device_lib
-from lib.code2features import TokenType
-import json
+
 
 def load_data(path, files_count):
     dirs = [x for x in os.listdir(path) if os.path.isdir(os.path.join(path, x))]
@@ -27,11 +27,32 @@ def load_data(path, files_count):
     return blocks, labels
 
 
-if __name__ == "__main__":
+def calc_metrics(real_answers, answers):
+    tp = 0.00001
+    tn = 0.00001
+    fp = 0.00001
+    fn = 0.00001
+
+    for j, val in enumerate(answers):
+        if real_answers[j] > 0.5:
+            is_valid = 1 if val > 0.5 else 0
+            tp += is_valid
+            fn += 1 - is_valid
+        else:
+            is_valid = 1 if val < 0.5 else 0
+            tn += is_valid
+            fp += 1 - is_valid
+    accuracy = (tp + tn) / (tp + tn + fp + fn)
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+
+    return accuracy, precision, recall
+
+
+def main():
     def get_available_gpus():
         local_device_protos = device_lib.list_local_devices()
         return [x.name for x in local_device_protos if x.device_type == 'GPU']
-
 
     target_device = "/gpu:0" if get_available_gpus() else "/cpu:0"
     print(target_device)
@@ -40,6 +61,8 @@ if __name__ == "__main__":
     parser.add_argument("-d", required=True, dest="dataset_dir", help="Input dataset path")
     parser.add_argument("-s", required=False, dest="files_count", help="Repository files count", default=10)
     parser.add_argument("-o", required=True, dest="output_file", help="Output model")
+    parser.add_argument("-v", required=False, type=float, dest="validation_prc", help="Validation %", default=0.0)
+    parser.add_argument("-r", required=False, type=int, dest="random_seed", help="Random seed for validation", default=1234567)
     args = parser.parse_args()
 
     data_vecs, data_labels = load_data(args.dataset_dir, args.files_count)
@@ -47,35 +70,44 @@ if __name__ == "__main__":
     deltas = [np.array(v1) - np.array(v2) for v1 in data_vecs for v2 in data_vecs]
     answers_raw = [1.0 if a1 == a2 else 0.0 for a1 in data_labels for a2 in data_labels]
 
-    mean_answers = np.mean(answers_raw)
+    random.Random(x=args.random_seed).shuffle(deltas)
+    random.Random(x=args.random_seed).shuffle(answers_raw)
+
+    validation_size = int(len(deltas) * args.validation_prc * 100)
+    deltas_valid = deltas[0:validation_size]
+    answers_raw_valid = answers_raw[0:validation_size]
+    deltas = deltas[validation_size:]
+    answers_raw = answers_raw[validation_size:]
 
     with tf.Session() as sess:
         with tf.device(target_device):
-            model = lib.authorchecker.Checker(len(deltas[0]), constant_input=np.array(deltas))
+            model = lib.authorchecker.Checker(len(deltas[0]))
+
+            model_answers = model.apply(tf.constant(deltas, dtype=tf.float32))
             answers = tf.constant(answers_raw)
-            loss = tf.reduce_mean(tf.pow(tf.subtract(model.get_answer(), answers), 2.0))
+            loss = tf.reduce_mean(tf.pow(tf.subtract(model_answers, answers), 2.0))
+
+            model_answers_valid = model.apply(tf.constant(deltas_valid, dtype=tf.float32))
 
             optimizer = tf.train.AdamOptimizer(learning_rate=0.1).minimize(loss)
             tf.global_variables_initializer().run()
             for i in range(10000):
                 _, loss_val, answer_val = sess.run((optimizer, loss, model.answer))
-                tp = 0.00001
-                tn = 0.00001
-                fp = 0.00001
-                fn = 0.00001
-                for j, v in enumerate(answer_val):
-                    if answers_raw[j] > 0.5:
-                        is_valid = 1 if v > 0.5 else 0
-                        tp += is_valid
-                        fn += 1 - is_valid
-                    else:
-                        is_valid = 1 if v < 0.5 else 0
-                        tn += is_valid
-                        fp += 1 - is_valid
-                accuracy = (tp + tn) / (tp + tn + fp + fn)
-                precision = tp / (tp + fp)
-                recall = tp / (tp + fn)
+                accuracy, precision, recall = calc_metrics(answers_raw, answer_val)
                 if i % 100 == 0:
+                    if validation_size:
+                        answer_val_valid = sess.run(model_answers_valid)
+                        accuracy_valid, precision_valid, recall_valid = calc_metrics(answers_raw_valid, answer_val_valid)
+                        print("{}. VALIDATION accuracy={}, precision={}, recall={}".format(
+                                i,
+                                accuracy_valid,
+                                precision_valid,
+                                recall_valid
+                            )
+                        )
                     model.save(sess, args.output_file)
                 print("{}. loss={}, accuracy={}, precision={}, recall={}".format(i, loss_val, accuracy, precision, recall))
         model.save(sess, args.output_file)
+
+if __name__ == "__main__":
+    main()
--- src/lib/authorchecker.py
+++ src/lib/authorchecker.py
@@ -3,12 +3,8 @@ from code2features import TokenType
 
 
 class Checker(object):
-    def __init__(self, index_size=TokenType.size * TokenType.size, constant_input=None):
+    def __init__(self, index_size=TokenType.size * TokenType.size):
         checker_classes = 10
-        if constant_input is not None:
-            self.delta_input = tf.constant(constant_input, dtype=tf.float32)
-        else:
-            self.delta_input = tf.placeholder(shape=(None, index_size), dtype=tf.float32)
         initializer = tf.truncated_normal_initializer(mean=0.0, stddev=1, seed=1234567, dtype=tf.float32)
 
         self.weights = tf.get_variable(
@@ -38,15 +34,16 @@ class Checker(object):
         with tf.device("/cpu:0"):
             self.saver = tf.train.Saver()
 
-        # x = tf.reduce_sum(tf.abs(tf.multiply(self.delta_input, self.weights)), axis=1)
-        # l = tf.nn.sigmoid(tf.add(x, self.bias))
-        # self.answer = tf.subtract(1.0, l)
-        x0 = tf.matmul(tf.abs(self.delta_input), tf.abs(self.weights))
+        self.delta_input = tf.placeholder(shape=(None, index_size), dtype=tf.float32)
+
+        self.answer = self.apply(self.delta_input)
+
+    def apply(self, value):
+        x0 = tf.matmul(tf.abs(value), tf.abs(self.weights))
         x = tf.nn.sigmoid(tf.add(x0, self.bias))
         t0 = tf.matmul(x, self.postweight)
         t = tf.nn.sigmoid(tf.add(t0, self.postbias))
-
-        self.answer = tf.subtract(1.0, tf.reduce_mean(t, axis=1))
+        return tf.subtract(1.0, tf.reduce_mean(t, axis=1))
 
     def save(self, sess, path):
         self.saver.save(sess, path)
