0ae8c89d24cca867372152287d3f0f59bc6e9a45
Maxim <freepvps@gmail.com>
2018-05-13 07:43:45 +0300

    window size 20, vecsize 10

--- src/lib/index_maker/__init__.py
+++ src/lib/index_maker/__init__.py
@@ -31,7 +31,7 @@ def make_index(sentences):
     sigm = math.sqrt(10)
 
     features = [map(str, code2features.extract_features(sentence)) for sentence in sentences]
-    w2v_model = Word2Vec(features, size=20, window=10, min_count=2, workers=1)
+    w2v_model = Word2Vec(features, size=10, window=20, min_count=2, workers=1)
 
     vectors = map(np.array, w2v_model.wv.vectors)
     labels = [int(w2v_model.wv.index2word[i]) for i in range(len(vectors))]
