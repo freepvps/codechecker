624647a50f1398762d1924666e8c64dfb61a151f
Maxim <freepvps@gmail.com>
2018-05-14 02:57:48 +0300

    filter tokens

--- src/lib/index_maker.py
+++ src/lib/index_maker.py
@@ -20,6 +20,34 @@ def distance_to_probability(v, sigm=1.32):
     return res
 
 
+used_tokens = [
+    code2features.TokenType.is_comma,
+    code2features.TokenType.is_dot,
+    code2features.TokenType.is_dotcomma,
+    code2features.TokenType.is_float_numeric,
+    code2features.TokenType.is_long_comment,
+    code2features.TokenType.is_long_lower_word,
+    code2features.TokenType.is_long_upper_word,
+    code2features.TokenType.is_lower_case_word,
+    code2features.TokenType.is_lower_case_word_,
+    code2features.TokenType.is_numeric,
+    code2features.TokenType.is_op_assert,
+    code2features.TokenType.is_op_binmath,
+    code2features.TokenType.is_op_extends,
+    code2features.TokenType.is_op_import,
+    code2features.TokenType.is_op_interface,
+    code2features.TokenType.is_op_package,
+    code2features.TokenType.is_op_protected,
+    code2features.TokenType.is_op_while,
+    code2features.TokenType.is_short_comment,
+    code2features.TokenType.is_short_word,
+    code2features.TokenType.is_unknown,
+    code2features.TokenType.is_very_short_word
+]
+token_to_id = {
+    t: i for i, t in enumerate(used_tokens)
+}
+
 def make_index(sentences):
     """
     :type sentences: list[basestring]
@@ -33,18 +61,20 @@ def make_index(sentences):
     vectors = map(np.array, w2v_model.wv.vectors)
     labels = [int(w2v_model.wv.index2word[i]) for i in range(len(vectors))]
 
-    features_size = code2features.TokenType.size
+    features_size = len(used_tokens)
     features_vecs = [None] * features_size
     for i, v in enumerate(vectors):
-        features_vecs[labels[i]] = v
+        real_id = token_to_id.get(labels[i])
+        if real_id is not None:
+            features_vecs[real_id] = v
 
     dist_matrix = np.zeros((features_size, features_size), dtype=np.float32)
     dists_list = []
     for i, v1 in enumerate(features_vecs):
         for j, v2 in enumerate(features_vecs):
-            if features_vecs[i] is None and features_vecs[j] is None:
+            if v1 is None and v2 is None:
                 dist_matrix[i][j] = 0.0
-            elif features_vecs[i] is None or features_vecs[j] is None:
+            elif v1 is None or v2 is None:
                 dist_matrix[i][j] = 100000.0
             else:
                 dist_matrix[i][j] = np.linalg.norm(v1 - v2)
