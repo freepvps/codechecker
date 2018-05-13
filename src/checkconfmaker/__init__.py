419acf5e0806eda8168c3c2d4effa9ab0bae602c
Maxim <freepvps@gmail.com>
2018-05-13 04:35:07 +0300

    feature analyze

--- src/checkconfmaker/__init__.py
+++ src/checkconfmaker/__init__.py
@@ -28,6 +28,7 @@ def load_data(path, files_count):
 
 
 if __name__ == "__main__":
+    print(json.dumps(TokenType.names))
     def get_available_gpus():
         local_device_protos = device_lib.list_local_devices()
         return [x.name for x in local_device_protos if x.device_type == 'GPU']
